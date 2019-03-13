69893586416b35f45496f20154b8496e5b5519b2
Han Liu <gottecho1992@gmail.com>
2019-03-13 10:58:51 +0800

    New Tool: Dark Window with some override style
    Dark window style
    A corresponding scrollview
    A corresponding dropdown box
    A circle buttom
    A corresponding toobar

---
+++ DarkWinStyle/MainWinStyle/Class1.cs
@@ -0,0 +1,46 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////
+///        Name space: NewUI.MainView.MainWinStyle
+///        File name : Class1
+///        Description:
+///        Version Nr:
+///        Created data : 12/6/2018 9:01:02 AM
+///        
+///        Author: Han Liu
+///        Copyright (c) 2018 All Rights Reserved  
+///        
+/// Version History:
+///*********************************************************************************
+///        Date       Version        Author          Description
+///*********************************************************************************
+///   | xx.xx.xxxx |    1.0    |     Han Liu   |  Initial Version:
+///  
+/// //////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewUI.MainView.MainWinStyle
+{
+    class Class1
+    {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+///***************************************************************************************
+/// Automation part...
+///***************************************************************************************
