44af9c3606134341a2d835406081da3ff07ed3b5
Han Liu <gottecho1992@gmail.com>
2019-10-15 11:03:13 +0800

    Add GeoMath: Vector and line functions here
     line functions: FindDistanceToSegment
    LineSegementsIntersect

---
+++ GeoMathFunctions/VectorMath/LineSegment.cs
@@ -0,0 +1,135 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////
+///        Name space: NibotUI.Business.VectorMath
+///        File name : LineSegment
+///        Description:
+///        Version Nr:
+///        Created data : 10/15/2019 10:43:33 AM
+///        
+///        Author: Han Liu
+///        Copyright (c) 2018 All Rights Reserved  
+///        
+/// Version History:
+///*********************************************************************************
+///        Date       Version        Author          Description
+///*********************************************************************************
+///   | xx.xx.xxxx |    1.0    |     Han Liu   |  Initial Version:
+///  
+/// //////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NibotUI.Business.VectorMath
+{
+    public class LineSegment
+    {
+        /// <summary>
+        /// Test whether two line segments intersect. If so, calculate the intersection point.
+        /// <see cref="http://stackoverflow.com/a/14143738/292237"/>
+        /// </summary>
+        /// <param name="p">Vector to the start point of p.</param>
+        /// <param name="p2">Vector to the end point of p.</param>
+        /// <param name="q">Vector to the start point of q.</param>
+        /// <param name="q2">Vector to the end point of q.</param>
+        /// <param name="intersection">The point of intersection, if any.</param>
+        /// <param name="considerOverlapAsIntersect">Do we consider overlapping lines as intersecting?</param>
+        /// <returns>True if an intersection point was found.</returns>
+        public static bool LineSegementsIntersect(Vector p, Vector p2, Vector q, Vector q2, out Vector intersection, bool considerOverlapAsIntersect = false)
+        {
+            intersection = new Vector();
+
+            var r = p2 - p;
+            var s = q2 - q;
+            var rxs = r.Cross(s);
+            var qpxr = (q - p).Cross(r);
+
+            // If r x s = 0 and (q - p) x r = 0, then the two lines are collinear.
+            if (rxs.IsZero() && qpxr.IsZero())
+            {
+                // 1. If either  0 <= (q - p) * r <= r * r or 0 <= (p - q) * s <= * s
+                // then the two lines are overlapping,
+                if (considerOverlapAsIntersect)
+                    if ((0 <= (q - p) * r && (q - p) * r <= r * r) || (0 <= (p - q) * s && (p - q) * s <= s * s))
+                        return true;
+
+                // 2. If neither 0 <= (q - p) * r ≤ r * r nor 0 <= (p - q) * s <= s * s
+                // then the two lines are collinear but disjoint.
+                // No need to implement this expression, as it follows from the expression above.
+                return false;
+            }
+
+            // 3. If r x s = 0 and (q - p) x r != 0, then the two lines are parallel and non-intersecting.
+            if (rxs.IsZero() && !qpxr.IsZero())
+                return false;
+
+            // t = (q - p) x s / (r x s)
+            var t = (q - p).Cross(s) / rxs;
+
+            // u = (q - p) x r / (r x s)
+
+            var u = (q - p).Cross(r) / rxs;
+
+            // 4. If r x s != 0 and 0 <= t <= 1 and 0 <= u <= 1
+            // the two line segments meet at the point p + t r = q + u s.
+            if (!rxs.IsZero() && (0 <= t && t <= 1) && (0 <= u && u <= 1))
+            {
+                // We can calculate the intersection point using either t or u.
+                intersection = p + t * r;
+
+                // An intersection was found.
+                return true;
+            }
+
+            // 5. Otherwise, the two line segments are not parallel but do not intersect.
+            return false;
+        }
+
+
+        // Calculate the distance between
+        // point pt and the segment p1 --> p2.
+        private double FindDistanceToSegment(Vector pt, Vector p1, Vector p2, out Vector closest)
+        {
+            var dx = p2.X - p1.X;
+            var dy = p2.Y - p1.Y;
+            if ((dx == 0) && (dy == 0))
+            {
+                // It's a point not a line segment.
+                closest = p1;
+                dx = pt.X - p1.X;
+                dy = pt.Y - p1.Y;
+                return Math.Sqrt(dx * dx + dy * dy);
+            }
+
+            // Calculate the t that minimizes the distance.
+            var t = ((pt.X - p1.X) * dx + (pt.Y - p1.Y) * dy) /
+                (dx * dx + dy * dy);
+
+            // See if this represents one of the segment's
+            // end points or a point in the middle.
+            if (t < 0)
+            {
+                closest = new Vector(p1.X, p1.Y);
+                dx = pt.X - p1.X;
+                dy = pt.Y - p1.Y;
+            }
+            else if (t > 1)
+            {
+                closest = new Vector(p2.X, p2.Y);
+                dx = pt.X - p2.X;
+                dy = pt.Y - p2.Y;
+            }
+            else
+            {
+                closest = new Vector(p1.X + t * dx, p1.Y + t * dy);
+                dx = pt.X - closest.X;
+                dy = pt.Y - closest.Y;
+            }
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
---
+++ GeoMathFunctions/VectorMath/Vector.cs
@@ -0,0 +1,88 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////
+///        Name space: NibotUI.Business.VectorMath
+///        File name : Vector
+///        Description:
+///        Version Nr:
+///        Created data : 10/15/2019 10:42:40 AM
+///        
+///        Author: Han Liu
+///        Copyright (c) 2018 All Rights Reserved  
+///        
+/// Version History:
+///*********************************************************************************
+///        Date       Version        Author          Description
+///*********************************************************************************
+///   | xx.xx.xxxx |    1.0    |     Han Liu   |  Initial Version:
+///  
+/// //////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NibotUI.Business.VectorMath
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+
+        // Constructors.
+        public Vector(double x, double y) { X = x; Y = y; }
+        public Vector() : this(double.NaN, double.NaN) { }
+
+        public static Vector operator -(Vector v, Vector w)
+        {
+            return new Vector(v.X - w.X, v.Y - w.Y);
+        }
+
+        public static Vector operator +(Vector v, Vector w)
+        {
+            return new Vector(v.X + w.X, v.Y + w.Y);
+        }
+
+        public static double operator *(Vector v, Vector w)
+        {
+            return v.X * w.X + v.Y * w.Y;
+        }
+
+        public static Vector operator *(Vector v, double mult)
+        {
+            return new Vector(v.X * mult, v.Y * mult);
+        }
+
+        public static Vector operator *(double mult, Vector v)
+        {
+            return new Vector(v.X * mult, v.Y * mult);
+        }
+
+        public double Cross(Vector v)
+        {
+            return X * v.Y - Y * v.X;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var v = (Vector)obj;
+            return (X - v.X).IsZero() && (Y - v.Y).IsZero();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0};{1})", X, Y);
+        }
+    }
+
+    public static class Extensions
+    {
+        private const double Epsilon = 1e-10;
+
+        public static bool IsZero(this double d)
+        {
+            return Math.Abs(d) < Epsilon;
+        }
+    }
+}
